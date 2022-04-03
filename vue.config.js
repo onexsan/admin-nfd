01b2c1924de474c9a70341404b2e31a5280b83d1
Victoria Filileeva <onexsan@gmail.com>
2022-04-03 21:02:40 +0300

    V-07 Исправление пути к gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/need-for-drive/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/admin-nfd/' : '/',
   css: {
     loaderOptions: {
       sass: {
