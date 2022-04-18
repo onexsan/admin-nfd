188e6d73d7295c91e8c923c3df7a4d88a93db833
Victoria Filileeva <onexsan@gmail.com>
2022-04-18 12:25:01 +0300

    V-10 Исправление порядка props, добавление ссылок в навигацию

--- src/router/index.js
+++ src/router/index.js
@@ -13,13 +13,6 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    redirect: () => {
-      return { path: '/login/' };
-    },
-  },
-  {
     path: '/admin/order-list',
     name: 'OrderList',
     components: {
